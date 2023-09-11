8fcb1f087f39c9d48a93814a756118d9632d3e27
M.Mostova <mmainfo15@gmail.com>
2023-09-12 00:27:28 +0300

    add styles for footer

--- footer.php
+++ footer.php
@@ -1,5 +1,24 @@
+
+<div class="pop-up">
+    <svg class="button-menu__icon" width="44px" height="44px" aria-label="Перемикач мобільного меню">
+      <use class="button-menu__cros" href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-x"></use>
+    </svg>
+    <p class="pop-up__greetings">Вітаємо!</p>
+    <p class="pop-up__text">Ви успішно підписалися на розсилку</p>
+    <div  class="pop-up__img">
+          <img  src="<?php bloginfo('template_url'); ?>/assets/images/pov-woman-feeds-wild-horse 1.jpg" alt="Жінка з рук кормить коня ">
+    </div>
+    <a href="<?php the_permalink(); ?>" class="pop-up__link"">До Подій    
+       <svg class="pop-up__icon" width="24px" height="24px">
+          <use href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-arrow"></use>
+       </svg>
+    </a>
+</div>
+
 <footer class="footer">
     <div class="container section">
+
+    <div class="footer__wrapper">
 
        <div class="footer-menu">
             <div class="footer-menu__logo">
@@ -19,13 +38,13 @@
                  ] ); 
              ?>   
 
-        </div>
+       </div>
 
         <div class="footer-info">
-            
-            <?php echo do_shortcode('[contact-form-7 id="e76af3c" title="Контактна форма"]'); ?>
-                  
-                       <ul class="footer-networks">
+
+           <?php echo do_shortcode('[contact-form-7 id="e76af3c" title="Контактна форма"]'); ?>
+
+                             <ul class="footer-networks">
                                 <li class="footer-networks__item" >
                                     <a href="#">
                                     <svg class="footer-networks__icon" width="44px" height="44px">
@@ -60,9 +79,13 @@
                                 </ul>
         </div>
 
-
     </div>
-</footer>
+ 
+    <span class="footer__rigths"> &copy 2023 Щербаті Цуглі. All rights reserved.</span>
+    </div>
+
+
+    </footer>
 <?php wp_footer(); ?>  
 </body>
 </html>
--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,1 +1,22 @@
-console.log('footer part');
+function handleWpcf7Invalid(event) {
+  event.preventDefault();
+
+  const errorMessage = event.detail.apiResponse.message;
+
+  Toastify({
+    text: errorMessage,
+    duration: 5000,
+    gravity: "bottom",
+    position: "center",
+    style: {
+      background: "#4f4840",
+      color: "#f1eee8",
+      fontSize: "16px",
+      borderRadius: "8px",
+    },
+  }).showToast();
+}
+
+document.addEventListener("wpcf7invalid", handleWpcf7Invalid, false);
+document.addEventListener("wpcf7mailfailed", handleWpcf7Invalid, false);
+// document.addEventListener("wpcf7mailsent", handleWpcf7Invalid, false);
