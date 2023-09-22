e3bd8830d8705f6daa08fd1641edb061482274a9
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-09-22 17:33:25 +0200

    add markup and styles for contact form with map

--- templates/contacts.php
+++ templates/contacts.php
@@ -5,10 +5,11 @@
 	get_header();
 ?>
 
-
 <main class="container">
+
     <h1 class='visually-hidden'>Зв'яжіться з нами</h1>
 	<?php get_template_part( 'template-parts/hero' ); ?>
+
 
     <!--        Social links-->
     <section class="section">
@@ -78,6 +79,43 @@
             </ul>
         </div>
     </section>
+
+    <!--    Contact form + location map-->
+    <section class="section">
+        <div class="contact">
+            <div class="location-wrapper">
+                <div class="address">
+                <span>
+                <svg>
+                    <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-location"></use>
+                </svg>
+                </span>
+                    <p>Хмельницька область, 32432, Кам'янець-Подільський р-н,
+                        с. Слобідка-Маліївецька, вул. Лісова 23.
+                    </p>
+                </div>
+
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d21028.16555892187!2d27.027787656674914!3d48.98766603035668!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473246634da3ed57%3A0xa32610c2812f5a40!2sSlobidka-Malijevets&#39;ka%2C%20Khmelnytskyi%20Oblast%2C%20Ukraine%2C%2032432!5e0!3m2!1sen!2spl!4v1695319573377!5m2!1sen!2spl"
+                            title="Розташування стайні Щербаті цуглі"
+                            width="100%"
+                            height="100%"
+                            style="border:0;" allowfullscreen loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </div>
+            </div>
+
+            <div class="form-wrapper">
+                <h2 class="form-title">Контактна форма</h2>
+                <p class="form-text">Якщо Ви маєте коментарі чи запитання, зв`яжіться з нами зручним способом.</p>
+
+				<?php echo do_shortcode( '[contact-form-7 id="01ba850" title="Контактна форма - сторінка контактів"]' ); ?>
+            </div>
+        </div>
+    </section>
+
+
 </main>
 
 <?php get_footer(); ?>
