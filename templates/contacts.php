a548c8b696ae7660fff7ce0e0fc5511d4d13cb09
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-09-25 11:29:09 +0200

    add dynamic data from wordpress

--- templates/contacts.php
+++ templates/contacts.php
@@ -10,70 +10,73 @@
     <h1 class='visually-hidden'>Зв'яжіться з нами</h1>
 	<?php get_template_part( 'template-parts/hero' ); ?>
 
-
     <!--        Social links-->
     <section class="section">
         <div class="socials">
             <ul>
                 <li class="social-box">
                     <div class="messengers">
-                        <a href="viber://chat?number=+380978955553" rel="noopener noreferrer" target="_blank">
+                        <a href="viber://chat?number=<?php the_field( 'messengers' ); ?>" rel="noopener noreferrer"
+                           target="_blank">
                             <svg>
                                 <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-viber"></use>
                             </svg>
                         </a>
-                        <a href="https://t.me/+30978955553" rel="noopener noreferrer" target="_blank">
+                        <a href="https://t.me/<?php the_field( 'messengers' ); ?>" rel="noopener noreferrer"
+                           target="_blank">
                             <svg>
                                 <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-telegram"></use>
                             </svg>
                         </a>
-                        <a href="https://wa.me/+380978955553" rel="noopener noreferrer" target="_blank">
+                        <a href="https://wa.me/<?php the_field( 'messengers' ); ?>" rel="noopener noreferrer"
+                           target="_blank">
                             <svg>
                                 <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-whatsapp"></use>
                             </svg>
                         </a>
                     </div>
-                    <span> (097) 895 55 53</span>
+                    <span> <?php the_field( 'messengers_display' ); ?></span>
                 </li>
 
-
                 <li>
-                    <a class="social-box" href="tel:+380937451349" target="_blank">
+                    <a class="social-box" href="tel:<?php the_field( 'telephone' ); ?>" target="_blank">
                         <svg>
                             <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-carbon_phone"></use>
                         </svg>
-                        <span>(093) 745 13 49</span>
+                        <span><?php the_field( 'telephone_display' ); ?></span>
                     </a>
                 </li>
 
                 <li>
-                    <a class="social-box" href="mailto:tsugli.ngo@gmail.com" rel="noopener noreferrer"
+                    <a class="social-box" href="mailto:<?php the_field( 'email' ); ?>" rel="noopener noreferrer"
                        target="_blank">
                         <svg>
                             <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-mail"></use>
                         </svg>
-                        <span>tsugli.ngo@gmail.com</span>
+                        <span><?php the_field( 'email' ); ?></span>
                     </a>
                 </li>
 
                 <li>
-                    <a class="social-box" href="https://www.instagram.com/shche_tsugli/" rel="noopener noreferrer"
+                    <a class="social-box" href="https://www.instagram.com/<?php the_field( 'instagram' ); ?>/"
+                       rel="noopener noreferrer"
                        target="_blank">
                         <svg>
                             <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-instagram"></use>
                         </svg>
-                        <span>shche_tsugli</span>
+                        <span><?php the_field( 'instagram' ); ?></span>
                     </a>
                 </li>
 
                 <li>
-                    <a class="social-box" href="https://www.facebook.com/shche.tsugli/" rel="noopener noreferrer"
+                    <a class="social-box" href="https://www.facebook.com/<?php the_field( 'facebook' ) ?>/"
+                       rel="noopener noreferrer"
                        target="_blank"
                     >
                         <svg>
                             <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-fb"></use>
                         </svg>
-                        <span>shche.tsugli</span>
+                        <span><?php the_field( 'facebook' ); ?></span>
                     </a>
                 </li>
             </ul>
@@ -90,13 +93,11 @@
                     <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-location"></use>
                 </svg>
                 </span>
-                    <p>Хмельницька область, 32432, Кам'янець-Подільський р-н,
-                        с. Слобідка-Маліївецька, вул. Лісова 23.
-                    </p>
+                    <p><?php the_field( 'address' ) ?></p>
                 </div>
 
                 <div class="map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d21028.16555892187!2d27.027787656674914!3d48.98766603035668!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473246634da3ed57%3A0xa32610c2812f5a40!2sSlobidka-Malijevets&#39;ka%2C%20Khmelnytskyi%20Oblast%2C%20Ukraine%2C%2032432!5e0!3m2!1sen!2spl!4v1695319573377!5m2!1sen!2spl"
+                    <iframe src="<?php the_field( 'address_link' ) ?>"
                             title="Розташування стайні Щербаті цуглі"
                             width="100%"
                             height="100%"
@@ -107,14 +108,13 @@
             </div>
 
             <div class="form-wrapper">
-                <h2 class="form-title">Контактна форма</h2>
-                <p class="form-text">Якщо Ви маєте коментарі чи запитання, зв`яжіться з нами зручним способом.</p>
+                <h2 class="form-title"><?php the_field( 'form_title' ) ?></h2>
+                <p class="form-text"><?php the_field( 'form_text' ) ?></p>
 
 				<?php echo do_shortcode( '[contact-form-7 id="01ba850" title="Контактна форма - сторінка контактів"]' ); ?>
             </div>
         </div>
     </section>
-
 
 </main>
 
