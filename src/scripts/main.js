680dc0de344e772fcef36a2403adfdf32aa0acab
M.Mostova <mmainfo15@gmail.com>
2023-09-28 16:48:42 +0300

    Fixed swiper file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -40,13 +40,3 @@ function onOpenModalHomeWindow(event) {
   }
 }
 
-// Swiper
-const articleSwiper = new Swiper(".swiper-article", {
-  slidesPerView: "auto",
-  spaceBetween: 16,
-  breakpoints: {
-    992: {
-      spaceBetween: 24,
-    },
-  },
-});
---
+++ src/scripts/single.js
@@ -0,0 +1,10 @@
+// Swiper
+const articleSwiper = new Swiper(".swiper-article", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  breakpoints: {
+    992: {
+      spaceBetween: 24,
+    },
+  },
+});
