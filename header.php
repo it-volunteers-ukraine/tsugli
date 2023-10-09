f4a59c8acba8a287c1845e6b1a31bcba790edf46
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-10-09 08:06:00 +0200

    update elements width

--- header.php
+++ header.php
@@ -1,95 +1,114 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset='<?php bloginfo('charset'); ?>' />
+<head>
+    <meta charset='<?php bloginfo( 'charset' ); ?>'/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <?php wp_head(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
     <title>Щербаті Цуглі</title>
 </head>
-<body>  
-    <div class="wrapper">
-        <header class="header">
-            <div class="container section">
-                    <div class="header__wrapper">  
-                    <?php 
-                        if ( has_custom_logo() ) {
-                            echo get_custom_logo();
-                        }
-                    ?>  
-                <button  class="button-menu is-open"
-                    type="button"
-                    aria-controls="menu-container"
-                    data-menu-button>
+<body>
+<div class="wrapper">
+    <header class="header">
+        <div class="container section">
+
+
+            <div class="header__wrapper">
+				<?php
+					if ( has_custom_logo() ) {
+						echo get_custom_logo();
+					}
+				?>
+                <button class="button-menu is-open"
+                        type="button"
+                        aria-controls="menu-container"
+                        data-menu-button>
                     <svg class="button-menu__icon" width="44px" height="44px" aria-label="Перемикач мобільного меню">
-                        <use class="button-menu__burger" href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-menu"></use>
-                        <use class="button-menu__cros" href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-x"></use>
+                        <use class="button-menu__burger"
+                             href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-menu"></use>
+                        <use class="button-menu__cros"
+                             href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-x"></use>
                     </svg>
-                </button>     
-                                     
-                 <div class="menu-container" id="menu-container" data-menu>
+                </button>
 
-                    <nav class="nav-menu"> 
-                            <?php wp_nav_menu( [
-                                'theme_location'       => 'header',                          
-                                'container'            => false,                           
-                                'menu_class'           => 'nav-menu__list',
-                                'menu_id'              => false,    
-                                'echo'                 => true,                            
-                                'items_wrap'           => '<ul id="%1$s" class="header_list %2$s">%3$s</ul>'  
-                                ] ); 
-/*
-Temporary put # instead of <?php echo esc_attr(get_field('instagram_link', 16));?> in the a tags in mobile menu to prevent project crush when run locally.
-*/
-                            ?>   
-                    </nav> 
-                    <div class="header__mobile">
-                                <a class="header__button" href="<?php echo get_permalink(20); ?>"
-                                    >Задонатити
-                                </a>   
+                <div class="menu-container" id="menu-container" data-menu>
+                    <div class="menu-wrapper">
+                        <nav class="nav-menu">
+							<?php wp_nav_menu( [
+								'theme_location' => 'header',
+								'container'      => false,
+								'menu_class'     => 'nav-menu__list',
+								'menu_id'        => false,
+								'echo'           => true,
+								'items_wrap'     => '<ul id="%1$s" class="header_list %2$s">%3$s</ul>'
+							] );
+								/*
+								Temporary put # instead of <?php echo esc_attr(get_field('instagram_link', 16));?> in the a tags in mobile menu to prevent project crush when run locally.
+								*/
+							?>
+                        </nav>
 
-                                <ul class="header-networks">
-                                <li class="header-networks__item" >
-                                    <a href="<?php echo esc_attr(get_field('viber', 18));?>" aria-label="Посилання на вайбер">
-                                    <svg class="header-networks__icon" width="44px" height="44px">
-                                        <use href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-viber"></use>
-                                    </svg>
+                        <div class="header__mobile">
+                            <a class="header__button" href="<?php echo get_permalink( 20 ); ?>"
+                            >Задонатити
+                            </a>
+                            <ul class="header-networks">
+                                <li class="header-networks__item">
+                                    <a href="<?php echo esc_attr( get_field( 'viber', 18 ) ); ?>"
+                                       aria-label="Посилання на вайбер">
+                                        <svg class="header-networks__icon" width="44px" height="44px">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-viber"></use>
+                                        </svg>
                                     </a>
                                 </li>
                                 <li class="header-networks__item">
-                                    <a href="<?php echo esc_attr(get_field('facebook', 18));?>" target="_blank" aria-label="Посилання на фейсбук">
-                                    <svg class="header-networks__icon" width="44px" height="44px" >
-                                        <use href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-fb"></use>
-                                    </svg>
+                                    <a href="<?php echo esc_attr( get_field( 'facebook', 18 ) ); ?>" target="_blank"
+                                       aria-label="Посилання на фейсбук">
+                                        <svg class="header-networks__icon" width="44px" height="44px">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-fb"></use>
+                                        </svg>
                                     </a>
                                 </li>
                                 <li class="header-networks__item">
-                                    <a href="mailto: <?php echo esc_attr(get_field('email', 18));?>" aria-label="Посилання на адресу електорнної пошти"
-                                    ><svg class="header-networks__icon" width="44px" height="44px">
-                                        <use href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-mail"></use></svg></a>
-                                </li>
-                                <li class="header-networks__item">
-                                    <a href="<?php echo esc_attr(get_field('instagram', 18));?>"target="_blank" aria-label="Посилання на інстаграм">
-                                    <svg class="header-networks__icon" width="44px" height="44px">
-                                    <use href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-instagram"></use>
-                                    </svg>
+                                    <a href="mailto: <?php echo esc_attr( get_field( 'email', 18 ) ); ?>"
+                                       aria-label="Посилання на адресу електорнної пошти"
+                                    >
+                                        <svg class="header-networks__icon" width="44px" height="44px">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-mail"></use>
+                                        </svg>
                                     </a>
                                 </li>
                                 <li class="header-networks__item">
-                                    <a href="<?php echo esc_attr(get_field('whatsapp', 18));?>" target="_blank" aria-label="Посилання на вотсапп">
-                                    <svg class="header-networks__icon" width="44px" height="44px">
-                                        <use href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-whatsapp"></use></svg></a>
+                                    <a href="<?php echo esc_attr( get_field( 'instagram', 18 ) ); ?>" target="_blank"
+                                       aria-label="Посилання на інстаграм">
+                                        <svg class="header-networks__icon" width="44px" height="44px">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-instagram"></use>
+                                        </svg>
+                                    </a>
                                 </li>
-                                </ul>
-                                </div>
-                     </div>
-                    <a class="header__link" href="<?php echo get_permalink(20); ?>"
-                     >Задонатити
-                            <svg class="offering__icon" width="24px" height="24px">
-                                <use href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-arrow"></use>
-                            </svg>
-                    </a>
+                                <li class="header-networks__item">
+                                    <a href="<?php echo esc_attr( get_field( 'whatsapp', 18 ) ); ?>" target="_blank"
+                                       aria-label="Посилання на вотсапп">
+                                        <svg class="header-networks__icon" width="44px" height="44px">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-whatsapp"></use>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
                 </div>
+
+                <a class="header__link" href="<?php echo get_permalink( 20 ); ?>"
+                >Задонатити
+                    <svg class="offering__icon" width="24px" height="24px">
+                        <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-arrow"></use>
+                    </svg>
+                </a>
             </div>
-        </header>
-        <body <?php body_class(); ?>>
+
+
+        </div>
+    </header>
+    <body <?php body_class(); ?>>
--- templates/home.php
+++ templates/home.php
@@ -1,177 +1,182 @@
 <?php
-/*
-Template Name: home
-*/
-get_header();
+	/*
+	Template Name: home
+	*/
+	get_header();
 ?>
-<main class="main" >
-<section class="hero-home" style="background: linear-gradient(100deg, #D9D9D9 15.95%, rgba(217, 217, 217, 0.00) 57.19%), url(<?php the_field('hero-img') ?>); background-size: cover; background-repeat: no-repeat; background-position: center; ">
-   <div class="container section">
-       <h1 class="visually-hidden">Коні української Поліської породи</h1>
-       <h2 class="hero-home__title"><?php the_field('hero-title'); ?></h2>
-       <p class="hero__text"><?php the_field('hero-text'); ?></p>
-       <a class="hero__button" href="<?php echo get_permalink(7); ?>"><?php the_field('hero-button'); ?>
-           <svg class="hero__icon" width="24px" height="24px">
-               <use href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-arrow"></use>
-           </svg>
-       </a>
-   </div>
-</section> 
-
-<section class="activities container">
-     <div class="swiper">
-    <div class="activities__list swiper-wrapper">
-        
-        <div class="activities__item swiper-slide">
-        <div class="activities__img">
-            <img src='<?php  the_field('img_1')?>' alt='<?php  the_field('alt_1')?>'/>
+<main class="main">
+    <section class="hero-home"
+             style="background: linear-gradient(100deg, #D9D9D9 15.95%, rgba(217, 217, 217, 0.00) 57.19%), url(<?php the_field( 'hero-img' ) ?>); background-size: cover; background-repeat: no-repeat; background-position: center; ">
+        <div class="container section">
+            <h1 class="visually-hidden">Коні української Поліської породи</h1>
+            <h2 class="hero-home__title"><?php the_field( 'hero-title' ); ?></h2>
+            <p class="hero__text"><?php the_field( 'hero-text' ); ?></p>
+            <a class="hero__button" href="<?php echo get_permalink( 7 ); ?>"><?php the_field( 'hero-button' ); ?>
+                <svg class="hero__icon" width="24px" height="24px">
+                    <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/symbol-defs.svg#icon-arrow"></use>
+                </svg>
+            </a>
         </div>
-        <p class="activities__text"><?php  the_field('text_1')?></p>
-        </div>
+    </section>
 
-        <div class="activities__item swiper-slide" >
-        <div class="activities__img">
-            <img src='<?php  the_field('img_2')?>' alt='<?php  the_field('alt_2')?>'/>
-        </div>
-        <p class="activities__text"><?php  the_field('text_2')?></p>
-        </div>
+    <section class="activities container">
+        <div class="swiper">
+            <div class="activities__list swiper-wrapper">
 
-        <div class="activities__item swiper-slide">
-        <div class="activities__img">
-            <img src='<?php  the_field('img_3')?>' alt='<?php  the_field('alt_3')?>'/>
-        </div>
-        <p class="activities__text"><?php  the_field('text_3')?></p>
-        </div>
-
-</div> 
-     </div>
-</section>
-
-<section class="about-us section container">
-    <div class="about-us__container">
-        <div class="about-us__logo">
-            <img src="<?php bloginfo('template_url'); ?>/assets/images/logo.svg" alt="Логотип" class='about-us__sign'>
-            <img src="<?php bloginfo('template_url'); ?>/assets/images/text.svg" alt="Щербаті Цуглі" class='about-us__name'>
-        </div>
-     <p class="about-us__text"><?php the_field('about-us__text'); ?> </p>
-
-</div>
- </section>
-
- <section class="gallery-desktop  section container">
-   <div class="gallery-home__wrapper ">
-
-          <div class="gallery__head">
-              <h2 class="gallery__title"><?php the_field('gallery-text'); ?></h2>
-              <ul class="gallery__buttons">
-                <li>
-                  <button class="gallery__button" data-category="museum">Музей-Хаб</button>
-                </li>
-                <li>
-                  <button class="gallery__button" data-category="horses">Поліські Коні</button>
-                </li>
-                <li>
-                  <button class="gallery__button" data-category="events">Події</button>
-                </li>
-                <li>
-                  <button class="gallery__button" data-category="excursions">Екскурсії</button>
-                </li>
-                <li>
-                  <button class="gallery__button active" data-category="all">Всі</button>
-                </li>
-              </ul>
-           </div>
-
-
-
-<?php if(have_rows('gallery')):?>
-        <div class="gallery-home__container lightBox">
-        <?php while(have_rows('gallery')): the_row();?>
-                <?php   
-        $image=get_sub_field('img');
-        $category = get_sub_field('category'); 
-        $alt = get_sub_field('alt');
-      ?>
-
-          <a class="gallery__link gallery__item" data-category="<?php echo $category; ?>"  href="<?php echo $image['url']; ?>">
-            <img class="gallery__image" src="<?php echo $image['url']; ?>" alt="<?php echo $alt; ?>"  data-source="<?php echo $image['url']; ?>"/>
-          </a>
-
-<?php endwhile; ?>
-<?php endif; ?> 
-    </div>
-
-        <div class="gallery-pagination__box">
-          <ul class="gallery-pagination__list">
-              <?php
-              $total_images = count(get_field('gallery')); 
-              $images_per_page = 12; 
-              $total_pages = ceil($total_images / $images_per_page); 
-
-              for ($i = 1; $i <= $total_pages; $i++) {
-                  echo '<li><a  " data-page="' . $i . '">' . $i . '</a></li>';
-              }
-              ?>
-        </div>
-</section>
-
-
- <section class="gallery-mob section container">
-        <div class="gallery-home__wrapper ">
-          <h2 class="gallery__title"><?php the_field('gallery-text'); ?></h2>
-              <ul class="gallery__buttons">
-                <li>
-                  <button class="gallery__button" data-category="museum">Музей-Хаб</button>
-                </li>
-                <li>
-                  <button class="gallery__button" data-category="horses">Поліські Коні</button>
-                </li>
-                <li>
-                  <button class="gallery__button" data-category="events">Події</button>
-                </li>
-                <li>
-                  <button class="gallery__button" data-category="excursions">Екскурсії</button>
-                </li>
-                <li>
-                  <button class="gallery__button active" data-category="all">Всі</button>
-                </li>
-              </ul>
-              <div class="swiper-pagination"></div>
-
-<?php if (have_rows('gallery')) : ?>
-    <div class="galerry-home__container--mob swiper-gallery">
-        <div class="gallery__wrap swiper-wrapper">
-            <?php while (have_rows('gallery')) : the_row(); ?>
-                <?php
-                $image = get_sub_field('img');
-                $category = get_sub_field('category');
-                ?>
-
-                <div class="gallery__item swiper-slide" data-category="<?php echo $category; ?>">
-                    <img class="gallery__img" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" loading="lazy"/>
+                <div class="activities__item swiper-slide">
+                    <div class="activities__img">
+                        <img src='<?php the_field( 'img_1' ) ?>' alt='<?php the_field( 'alt_1' ) ?>'/>
+                    </div>
+                    <p class="activities__text"><?php the_field( 'text_1' ) ?></p>
                 </div>
-                
-            <?php endwhile; ?>
+
+                <div class="activities__item swiper-slide">
+                    <div class="activities__img">
+                        <img src='<?php the_field( 'img_2' ) ?>' alt='<?php the_field( 'alt_2' ) ?>'/>
+                    </div>
+                    <p class="activities__text"><?php the_field( 'text_2' ) ?></p>
+                </div>
+
+                <div class="activities__item swiper-slide">
+                    <div class="activities__img">
+                        <img src='<?php the_field( 'img_3' ) ?>' alt='<?php the_field( 'alt_3' ) ?>'/>
+                    </div>
+                    <p class="activities__text"><?php the_field( 'text_3' ) ?></p>
+                </div>
+
+            </div>
         </div>
-    </div>
-<?php endif; ?>
+    </section>
 
-</section>
+    <section class="about-us section container">
+        <div class="about-us__container">
+            <div class="about-us__logo">
+                <img src="<?php bloginfo( 'template_url' ); ?>/assets/images/logo.svg" alt="Логотип"
+                     class='about-us__sign'>
+                <img src="<?php bloginfo( 'template_url' ); ?>/assets/images/text.svg" alt="Щербаті Цуглі"
+                     class='about-us__name'>
+            </div>
+            <p class="about-us__text"><?php the_field( 'about-us__text' ); ?> </p>
 
-<section class="section events-section container">
+        </div>
+    </section>
+
+    <section class="gallery-desktop  section container">
+        <div class="gallery-home__wrapper ">
+
+            <div class="gallery__head">
+                <h2 class="gallery__title"><?php the_field( 'gallery-text' ); ?></h2>
+                <ul class="gallery__buttons">
+                    <li>
+                        <button class="gallery__button" data-category="museum">Музей-Хаб</button>
+                    </li>
+                    <li>
+                        <button class="gallery__button" data-category="horses">Поліські Коні</button>
+                    </li>
+                    <li>
+                        <button class="gallery__button" data-category="events">Події</button>
+                    </li>
+                    <li>
+                        <button class="gallery__button" data-category="excursions">Екскурсії</button>
+                    </li>
+                    <li>
+                        <button class="gallery__button active" data-category="all">Всі</button>
+                    </li>
+                </ul>
+            </div>
+
+
+			<?php if ( have_rows( 'gallery' ) ): ?>
+            <div class="gallery-home__container lightBox">
+				<?php while ( have_rows( 'gallery' ) ): the_row(); ?>
+					<?php
+					$image    = get_sub_field( 'img' );
+					$category = get_sub_field( 'category' );
+					$alt      = get_sub_field( 'alt' );
+					?>
+
+                    <a class="gallery__link gallery__item" data-category="<?php echo $category; ?>"
+                       href="<?php echo $image['url']; ?>">
+                        <img class="gallery__image" src="<?php echo $image['url']; ?>" alt="<?php echo $alt; ?>"
+                             data-source="<?php echo $image['url']; ?>"/>
+                    </a>
+
+				<?php endwhile; ?>
+				<?php endif; ?>
+            </div>
+
+            <div class="gallery-pagination__box">
+                <ul class="gallery-pagination__list">
+					<?php
+						$total_images    = count( get_field( 'gallery' ) );
+						$images_per_page = 12;
+						$total_pages     = ceil( $total_images / $images_per_page );
+
+						for ( $i = 1; $i <= $total_pages; $i ++ ) {
+							echo '<li><a  " data-page="' . $i . '">' . $i . '</a></li>';
+						}
+					?>
+            </div>
+    </section>
+
+
+    <section class="gallery-mob section container">
+        <div class="gallery-home__wrapper ">
+            <h2 class="gallery__title"><?php the_field( 'gallery-text' ); ?></h2>
+            <ul class="gallery__buttons">
+                <li>
+                    <button class="gallery__button" data-category="museum">Музей-Хаб</button>
+                </li>
+                <li>
+                    <button class="gallery__button" data-category="horses">Поліські Коні</button>
+                </li>
+                <li>
+                    <button class="gallery__button" data-category="events">Події</button>
+                </li>
+                <li>
+                    <button class="gallery__button" data-category="excursions">Екскурсії</button>
+                </li>
+                <li>
+                    <button class="gallery__button active" data-category="all">Всі</button>
+                </li>
+            </ul>
+            <div class="swiper-pagination"></div>
+
+			<?php if ( have_rows( 'gallery' ) ) : ?>
+                <div class="galerry-home__container--mob swiper-gallery">
+                    <div class="gallery__wrap swiper-wrapper">
+						<?php while ( have_rows( 'gallery' ) ) : the_row(); ?>
+							<?php
+							$image    = get_sub_field( 'img' );
+							$category = get_sub_field( 'category' );
+							?>
+
+                            <div class="gallery__item swiper-slide" data-category="<?php echo $category; ?>">
+                                <img class="gallery__img" src="<?php echo $image['url']; ?>"
+                                     alt="<?php echo $image['alt']; ?>" loading="lazy"/>
+                            </div>
+
+						<?php endwhile; ?>
+                    </div>
+                </div>
+			<?php endif; ?>
+
+    </section>
+
+    <section class="section events-section container">
 		<?php if ( have_posts() ) : ?>
 
-				<?php
+			<?php
 			while ( have_posts() ) :
 				the_post();
-				get_template_part( 'template-parts/content-events');
+				get_template_part( 'template-parts/content-events' );
 
 			endwhile;
 		endif;
 		?>
-</section>
+    </section>
 
-<?php get_template_part( 'template-parts/buttonTop'); ?>
+	<?php get_template_part( 'template-parts/buttonTop' ); ?>
 
 </main>
 
