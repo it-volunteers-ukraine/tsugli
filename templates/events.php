f512e5b75ece37db18aed80cacbeeb511f3d2dd3
M.Mostova <mmainfo15@gmail.com>
2023-10-31 13:43:49 +0200

    Update events.php

--- templates/events.php
+++ templates/events.php
@@ -8,16 +8,16 @@ get_header();
 
 <main class='main'>
     <h1 class='visually-hidden'>Найцікавіші події та заходи </h1>
-    
-  <?php get_template_part( 'template-parts/hero'); ?>
 
+    <?php get_template_part( 'template-parts/hero'); ?>
 
     <section class='events section container'>
         <p class="events-title"><?php the_field('text') ?></p>
+
         <div class='events__container'>
-<div class='events__posts'>
-	<?php if ( have_posts() ) : ?>
-			<?php
+            <div class='events__posts'>
+                <?php if ( have_posts() ) : ?>
+                <?php
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
@@ -26,42 +26,45 @@ get_header();
 			endwhile;
 		endif;
 		?>
-<?php
+                <?php
 
-$posts_per_page = 6;
+              $posts_per_page = 6;
 
-$total_posts = wp_count_posts()->publish;
+              $total_posts = wp_count_posts()->publish;
 
-// Вычисляем общее количество страниц пагинации
-$total_pages = ceil($total_posts / $posts_per_page);
+              if ($total_posts > 6):
+                  // Загальна кількість сторінок пагінації
+                  $total_pages = ceil($total_posts / $posts_per_page);
 
-// Получаем текущий номер страницы
-$current_page = get_query_var('paged') ? get_query_var('paged') : 1;
+                  // Отримуємо номер поточної сторінки
+                  $current_page = get_query_var('paged') ? get_query_var('paged') : 1;
 
-// Формируем массив с аргументами для функции paginate_links()
-$pagination_args = array(
-  'base' => get_pagenum_link(1) . '%_%',
-  'format' => '/page/%#%',
-  'current' => $current_page,
-  'total' => $total_pages,
-  'prev_next' => false,
-);
+                  // Формування масиву з аргументами для функції paginate_links()
+                  $pagination_args = array(
+                      'base' => get_pagenum_link(1) . '%_%',
+                      'format' => '/page/%#%',
+                      'current' => $current_page,
+                      'total' => $total_pages,
+                      'prev_next' => false,
+                  );
 
-// Выводим пагинацию
-echo '<div class="events__pagination">';
-echo paginate_links($pagination_args);
-echo '</div>';
-?>
-</div>
+                  // Виводимо пагінацію
+                  echo '<div class="events__pagination">';
+                  echo paginate_links($pagination_args);
+                  echo '</div>';
+                  
+                endif;
+              ?>
+            </div>
 
-<div class='events__sidebar'>
-    <?php get_sidebar(); ?>
-</div>
+            <div class='events__sidebar'>
+                <?php get_sidebar(); ?>
+            </div>
 
         </div>
     </section>
 
- <?php get_template_part( 'template-parts/buttonTop'); ?>
+    <?php get_template_part( 'template-parts/buttonTop'); ?>
 </main>
 
 <?php get_footer(); ?>
